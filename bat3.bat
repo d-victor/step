eecef359476fe767d07da2d38464605a4e6cf9ad
Pirog Vadim <vadim.pirog007@ukr.net>
2018-11-14 20:59:50 +0300

    3 ex

---
+++ bat3.bat
@@ -0,0 +1,2 @@
+@echo off
+(ping -n 1 %1 | find /i "TTL=") && (echo "%1 this good" >> C:\Users\Pirog\Desktop\exam\step\result.txt) || (echo "%1 this bad" >> C:\Users\Pirog\Desktop\exam\step\result.txt) & notepad C:\Users\Pirog\Desktop\exam\step\result.txt 
